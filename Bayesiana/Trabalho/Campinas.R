7b500b1622886c7287d9a85b31923d6fec234702
Caio Gomes Alves <caio.cga.cg@gmail.com>
2025-04-27 14:44:51 -0300

    Alterações no material de Prob.

--- Bayesiana/Trabalho/Campinas.R
+++ Bayesiana/Trabalho/Campinas.R
@@ -120,9 +120,25 @@ contagem_campinas$ea_u <- rep(1:17, 8)
 modelo_completo <- inla(Roubos ~ 1 +
                             f(ea_u, model = "bym", graph = mat_adj,
                               group = Ano2, control.group = list(model = "ar1")),
-                        data = contagem_campinas, E = E_campinas, family = "nbinomial",
+                        data = contagem_campinas, E = E_campinas, family = "poisson",
                         control.predictor = list(compute = TRUE, link = 1),
-                        control.compute = list(return.marginals.predictor = T))
+                        control.compute = list(return.marginals.predictor = T,
+                                               config = T,
+                                               dic = T))
+
+# Distribuições a posteriori para os valores dos hiper-parâmetros:
+lista_hiperparametros <- modelo_completo$marginals.hyperpar
+
+posterioris_hiper <- vector(mode = "list", length = length(lista_hiperparametros))
+
+for (i in 1:length(lista_hiperparametros)) {
+    posterioris_hiper[[i]] <- lista_hiperparametros[[i]] %>%
+        ggplot(aes(x = x, y = y)) +
+        geom_line() +
+        labs(title = names(lista_hiperparametros)[i])
+}
+
+ggpubr::ggarrange(plotlist = posterioris_hiper)
 
 # Valores ajustados para cada bairro e ano:
 modelo_completo$summary.fitted.values
@@ -137,17 +153,18 @@ contagem_campinas %>%
     scale_fill_distiller(palette = "Spectral") +
     facet_wrap(~Ano)
 
-# Diferença no risco relativo a priori e posteriori para Barão Geraldo:
+# Diferença no risco relativo a priori e posteriori para os bairros (removendo o Centro, pela escala):
 contagem_campinas %>%
-    filter(APG == "Barão Geraldo") %>%
-    select(Ano, RRAP, RRAPriori) %>%
-    ggplot(aes(x = Ano, y = RRAP, colour = "Priori")) +
+    filter(APG != "Centro") %>%
+    select(APG, Ano, RRAP, RRAPriori) %>%
+    ggplot(aes(x = Ano, y = RRAP, colour = "Posteriori")) +
     geom_line() +
     geom_point() +
-    geom_point(aes(y = RRAPriori, colour = "Posteriori")) +
-    geom_line(aes(y = RRAPriori, colour = "Posteriori")) +
+    geom_point(aes(y = RRAPriori, colour = "Priori")) +
+    geom_line(aes(y = RRAPriori, colour = "Priori")) +
     scale_y_continuous(breaks = seq(0.5, 1.5, by = 0.25)) +
     scale_colour_manual(values = c("darkgreen", "tomato")) +
+    facet_wrap(~APG) +
     labs(x = "Anos", y = "Risco Relativo", colour = "Risco")
 
 # Predição a Posteriori:
@@ -186,3 +203,124 @@ ggplot(contagem_campinas) +
     scale_fill_distiller(palette = "Spectral") +
     transition_time(Ano) +
     labs(title = "Ano: {round(frame_time, 0)}")
+
+
+#----Resíduos----
+
+# Resíduo por ano/bairro:
+contagem_campinas %>%
+    mutate(res = RRAP - RRAPriori) %>%
+    # group_by(APG) %>%
+    # summarise(res_med = mean(res)) %>%
+    ggplot(aes(fill = res)) +
+    geom_sf() +
+    facet_wrap(~Ano)
+
+# Resíduo médio por bairro:
+contagem_campinas %>%
+    mutate(res = RRAP - RRAPriori) %>%
+    group_by(APG) %>%
+    summarise(res_med = mean(res)) %>%
+    ggplot(aes(fill = res_med)) +
+    geom_sf()
+
+#----Predição----
+
+# Novos dados:
+campinas_2025 <- data.frame(
+    APG = contagem_campinas$APG[1:17],
+    POP_2022 = contagem_campinas$POP_2022[1:17],
+    geometry = contagem_campinas$geometry[1:17],
+    Roubos = NA,
+    Ano = rep(2025, 17),
+    RRAPriori = NA,
+    Ano2 = rep(9, 17),
+    ea_u = 1:17
+) %>%
+    st_as_sf()
+
+campinas_2025 <- campinas_2025 %>%
+    st_transform(crs = 31983)
+
+contagem_campinas_pred <- rbind(contagem_campinas, campinas_2025)
+
+E_campinas_pred <- numeric()
+
+for (i in 2017:2025) {
+    E_campinas_pred <- contagem_campinas_pred %>%
+        filter(Ano == i) %>%
+        func_expect() %>%
+        c(E_campinas_pred, .)
+}
+
+# Modelo:
+modelo_pred <- inla(Roubos ~ 1 +
+                        f(ea_u, model = "bym", graph = mat_adj,
+                          group = Ano2, control.group = list(model = "ar1")),
+                    data = contagem_campinas_pred, E = E_campinas_pred, family = "poisson",
+                    control.predictor = list(compute = TRUE, link = 1),
+                    control.compute = list(return.marginals.predictor = T,
+                                           config = T,
+                                           dic = T))
+
+# Distribuições a posteriori para os valores dos hiper-parâmetros:
+lista_hiperparametros_pred <- modelo_pred$marginals.hyperpar
+
+posterioris_hiper_pred <- vector(mode = "list", length = length(lista_hiperparametros_pred))
+
+for (i in 1:length(lista_hiperparametros_pred)) {
+    posterioris_hiper_pred[[i]] <- lista_hiperparametros_pred[[i]] %>%
+        ggplot(aes(x = x, y = y)) +
+        geom_line() +
+        labs(title = names(lista_hiperparametros_pred)[i])
+}
+
+ggpubr::ggarrange(plotlist = c(posterioris_hiper, posterioris_hiper_pred))
+
+# Valores ajustados para cada bairro e ano:
+modelo_pred$summary.fitted.values
+
+# Média do Risco Relativo a Posteriori:
+contagem_campinas_pred$RRAP <- modelo_pred$summary.fitted.values[, "mean"]
+
+# Distribuição espaço-temporal do risco relativo a posteriori:
+contagem_campinas_pred %>%
+    ggplot(aes(fill = RRAP)) +
+    geom_sf() +
+    scale_fill_distiller(palette = "Spectral") +
+    facet_wrap(~Ano)
+
+# Diferença no risco relativo a priori e posteriori para os bairros:
+contagem_campinas_pred %>%
+    # filter(APG == "Centro") %>%
+    select(APG, Ano, RRAP, RRAPriori) %>%
+    ggplot(aes(x = Ano, y = RRAP, colour = "Posteriori")) +
+    geom_line() +
+    geom_point() +
+    geom_point(aes(y = RRAPriori, colour = "Priori")) +
+    geom_line(aes(y = RRAPriori, colour = "Priori")) +
+    scale_y_continuous(breaks = seq(0.5, 1.5, by = 0.25)) +
+    scale_colour_manual(values = c("darkgreen", "tomato")) +
+    facet_wrap(~APG) +
+    labs(x = "Anos", y = "Risco Relativo", colour = "Risco")
+
+# Predição a Posteriori:
+
+# Densidades marginais do risco relativo a posteriori para Barão Geraldo:
+marginais_bg_pred <- list()
+
+for (i in 1:9) {
+    marginais_bg_pred[[i]] <- inla.smarginal(modelo_pred$marginals.fitted.values[[3 + 17 * (i - 1)]]) %>%
+        data.frame() %>%
+        mutate(Ano = 2017 + (i - 1))
+}
+
+marginais_bg_pred <- do.call(rbind, marginais_bg_pred)
+
+# Mudança no risco relativo a posteriori para Barão Geraldo:
+marginais_bg_pred %>%
+    ggplot(aes(x = x, y = y)) +
+    geom_line() +
+    facet_wrap(~Ano) +
+    labs(x = "Risco a posteriori", y = "Densidade") +
+    theme_bw()
